67ea1bc35ff82b0553ae569256701902e609e67f
ahmed <96090520+ahmedfathiaboelanin@users.noreply.github.com>
2022-08-28 07:13:50 +0200

    Update bmr.js

--- scripts/bmr.js
+++ scripts/bmr.js
@@ -12,11 +12,11 @@ function calculate(){
         else{
                 if(gender.value.toLowerCase().trim()=="male"){
                         if(fat=="" || fat.value > 13 || fat.value < 6){
-                                total.innerHTML="your BMR is : " + (+(+weight.value*10) + (6.25 * +length.value) - (5 * +age.value) + 5) + " Kcal";
+                                total.innerHTML="your BMR is : " + (+(+weight.value*10) + (6.25 * +length.value) - (5 * +age.value) + 5).toFixed(3) + " Kcal";
                                 total.style.backgroundColor="green";
                         }
                         else if(fat.value <= 13 && fat.value >= 6){
-                                total.innerHTML="Your BMR is : " + (370 + (21.6*(1 - (+fat.value /100))* +weight.value)) + " Kcal";
+                                total.innerHTML="Your BMR is : " + (370 + (21.6*(1 - (+fat.value /100))* +weight.value)).toFixed(3) + " Kcal";
                                 total.style.backgroundColor="green";
                         }
                         else{
@@ -26,11 +26,11 @@ function calculate(){
                 }
                 else if(gender.value.toLowerCase().trim()=="female"){
                         if(fat=="" || fat.value > 20 || fat.value < 14){
-                                total.innerHTML="Your BMR is : " + (+(+weight.value*10) + (6.25 * +length.value) - (5 * +age.value) - 161) + " Kcal";
+                                total.innerHTML="Your BMR is : " + (+(+weight.value*10) + (6.25 * +length.value) - (5 * +age.value) - 161).toFixed(3) + " Kcal";
                                 total.style.backgroundColor="green";
                         }
                         else if(fat.value <= 20 && fat.value >= 14){
-                                total.innerHTML= "Your BMR is : " + (370 + (21.6*(1 - (+fat.value /100))* +weight.value)) +" Kcal";
+                                total.innerHTML= "Your BMR is : " + (370 + (21.6*(1 - (+fat.value /100))* +weight.value)).toFixed(3) +" Kcal";
                                 total.style.backgroundColor="green";
                         }       
                 }
