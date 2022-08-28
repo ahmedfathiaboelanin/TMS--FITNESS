b4c619f73d300156c86c56534da2d8f96e5f8253
ahmed <96090520+ahmedfathiaboelanin@users.noreply.github.com>
2022-08-28 06:52:32 +0200

    Update bmr.js

--- scripts/bmr.js
+++ scripts/bmr.js
@@ -6,39 +6,40 @@ var fat=document.getElementById('fat');
 var total=document.getElementById('total');
 
 function calculate(){
-        if(gender.value=="male"){
-                if(typeof weight.value !== Number || typeof age.value !== Number || typeof length.value !== Number){
-                        alert("information required")
-                }
-                else if(fat=="" || fat.value > 13 || fat.value < 6){
-                        total.innerHTML="your BMR is : " + (+(+weight.value*10) + (6.25 * +length.value) - (5 * +age.value) + 5) + " Kcal";
-                        total.style.backgroundColor="green";
-                }
-                else if(fat.value <= 13 && fat.value >= 6){
-                        total.innerHTML="Your BMR is : " + (370 + (21.6*(1 - (+fat.value /100))* +weight.value)) + " Kcal";
-                        total.style.backgroundColor="green";
-                }
-                else{
-                        total.style.backgroundColor="red";
-                        total.innerHTML= "Your BMR is :";
-                }
-                
-        }
-        else if(gender.value=="female"){
-                if(typeof weight.value !== Number || typeof age.value !== Number || typeof length.value !== Number){
-                        alert("information required")
-                }
-                else if(fat=="" || fat.value > 20 || fat.value < 14){
-                        total.innerHTML="Your BMR is : " + (+(+weight.value*10) + (6.25 * +length.value) - (5 * +age.value) - 161) + " Kcal";
-                        total.style.backgroundColor="green";
-                }
-                else if(fat.value <= 20 && fat.value >= 14){
-                        total.innerHTML= "Your BMR is : " + (370 + (21.6*(1 - (+fat.value /100))* +weight.value)) +" Kcal";
-                        total.style.backgroundColor="green";
-                }
-                
+        if(weight.value == 0 || age.value == 0 || length.value == 0){
+                alert("information required")
         }
         else{
-                alert("Hint: information required");
+                if(gender.value.toLowerCase().trim()=="male"){
+                        if(fat=="" || fat.value > 13 || fat.value < 6){
+                                total.innerHTML="your BMR is : " + (+(+weight.value*10) + (6.25 * +length.value) - (5 * +age.value) + 5) + " Kcal";
+                                total.style.backgroundColor="green";
+                        }
+                        else if(fat.value <= 13 && fat.value >= 6){
+                                total.innerHTML="Your BMR is : " + (370 + (21.6*(1 - (+fat.value /100))* +weight.value)) + " Kcal";
+                                total.style.backgroundColor="green";
+                        }
+                        else{
+                                total.style.backgroundColor="red";
+                                total.innerHTML= "Your BMR is :";
+                        }       
+                }
+                else if(gender.value.toLowerCase().trim()=="female"){
+                        if(fat=="" || fat.value > 20 || fat.value < 14){
+                                total.innerHTML="Your BMR is : " + (+(+weight.value*10) + (6.25 * +length.value) - (5 * +age.value) - 161) + " Kcal";
+                                total.style.backgroundColor="green";
+                        }
+                        else if(fat.value <= 20 && fat.value >= 14){
+                                total.innerHTML= "Your BMR is : " + (370 + (21.6*(1 - (+fat.value /100))* +weight.value)) +" Kcal";
+                                total.style.backgroundColor="green";
+                        }       
+                }
+                else{
+                        alert("Hint: information required");
+                }
         }
 }
+
+
+
+
